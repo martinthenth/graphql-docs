473c29400895ca93512025ec1e497e7f0367880c
Martin Nijboer <mrnijboer@gmail.com>
2024-03-09 03:05:36 +0100

    Move field rendering to ApiContentListItem

--- app/app/(docs)/content-list-item.tsx
+++ app/app/(docs)/content-list-item.tsx
@@ -1,13 +1,16 @@
+import { uuid4 } from "@/lib/uuid";
+import { APIType, APITypeField, APITypeFieldArgument } from "@/types";
 import { HTMLAttributes } from "react";
 import { CodeBlock } from "@/components/ui/codeblock";
 import { Flex } from "@/components/ui/flex";
 import { Grid } from "@/components/ui/grid";
 import { H3, H4, H5, P } from "@/components/ui/typography";
 
-interface ApiContentListItemProps extends HTMLAttributes<HTMLDivElement> {
-  title: string;
-  subtitle: string;
-  example: string;
+interface ApiContentListItemTypeProps extends HTMLAttributes<HTMLDivElement> {
+  definition: APIType;
+}
+interface ApiContentListItemActionProps extends HTMLAttributes<HTMLDivElement> {
+  definition: APITypeField;
 }
 interface ApiContentListItemFieldProps extends HTMLAttributes<HTMLDivElement> {
   name: string;
@@ -15,12 +18,15 @@ interface ApiContentListItemFieldProps extends HTMLAttributes<HTMLDivElement> {
   description: string | null;
 }
 
-export function ApiContentListItem({
-  children,
-  example,
-  subtitle,
-  title,
-}: ApiContentListItemProps) {
+export function ApiContentListItemType({ definition }: ApiContentListItemTypeProps) {
+  const title = definition.description || "";
+  const subtitle = "Attributes";
+  const fields =
+    definition.fields && definition.fieldNames
+      ? definition.fieldNames.map((fieldName) => definition.fields![fieldName])
+      : [];
+  const example = buildExample(fields);
+
   return (
     <div>
       <H3>{title}</H3>
@@ -29,7 +35,16 @@ export function ApiContentListItem({
           <header className="py-2">
             <H4>{subtitle}</H4>
           </header>
-          <section>{children}</section>
+          <section>
+            {fields.map((field) => (
+              <ApiContentListItemField
+                key={field.name}
+                description={field.description}
+                name={field.name}
+                type={field.type}
+              />
+            ))}
+          </section>
         </Grid>
         <Grid>
           <CodeBlock content={example} language="json" />
@@ -39,7 +54,43 @@ export function ApiContentListItem({
   );
 }
 
-export function ApiContentListItemField({ description, name, type }: ApiContentListItemFieldProps) {
+export function ApiContentListItemAction({ definition }: ApiContentListItemActionProps) {
+  const title = definition.description || "";
+  const subtitle = "Parameters";
+  const fields =
+    definition.arguments && definition.argumentNames
+      ? definition.argumentNames.map((argumentName) => definition.arguments![argumentName])
+      : [];
+  const example = buildExample(fields);
+
+  return (
+    <div>
+      <H3>{title}</H3>
+      <Grid gap="md" items="start" className="grid-cols-2">
+        <Grid>
+          <header className="py-2">
+            <H4>{subtitle}</H4>
+          </header>
+          <section>
+            {fields.map((field) => (
+              <ApiContentListItemField
+                key={field.name}
+                description={field.description}
+                name={field.name}
+                type={field.type}
+              />
+            ))}
+          </section>
+        </Grid>
+        <Grid>
+          <CodeBlock content={example} language="json" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+function ApiContentListItemField({ description, name, type }: ApiContentListItemFieldProps) {
   return (
     <article className="border-t py-2">
       <Grid gap="xs">
@@ -55,4 +106,33 @@ export function ApiContentListItemField({ description, name, type }: ApiContentL
       </Grid>
     </article>
   );
+}
+
+function buildExample(fields: APITypeField[] | APITypeFieldArgument[]) {
+  let example: Record<string, any> = {};
+
+  fields.forEach((field) => {
+    const value = (() => {
+      switch (field.type) {
+        case "ID":
+          return "id";
+        case "UUID":
+          return uuid4();
+        case "Int":
+          return 42;
+        case "String":
+          return "string";
+        case "Boolean":
+          return true;
+        case "DateTime":
+          return new Date().toISOString();
+        default:
+          return null;
+      }
+    })();
+
+    example[field.name] = value;
+  });
+
+  return JSON.stringify(example, null, 2);
 }
--- app/app/(docs)/content-list.tsx
+++ app/app/(docs)/content-list.tsx
@@ -1,39 +1,13 @@
-import { uuid4 } from "@/lib/uuid";
-import { APIDocsArticle, APIType, APITypeField, APITypeFieldArgument } from "@/types";
+import { APIDocsArticle, APIType, APITypeField } from "@/types";
 import { HTMLAttributes } from "react";
 import { Grid } from "@/components/ui/grid";
 import { H2 } from "@/components/ui/typography";
-import { ApiContentListItem, ApiContentListItemField } from "./content-list-item";
+import { ApiContentListItemAction, ApiContentListItemType } from "./content-list-item";
 
 interface ApiContentListProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
   articles: APIDocsArticle[];
 }
-
-const code = `{
-  "id": "cus_NffrFeUfNV2Hib",
-  "object": "customer",
-  "address": null,
-  "balance": 0,
-  "created": 1680893993,
-  "currency": null,
-  "email": "jennyrosen@example.com",
-  "invoice_prefix": "0759376C",
-  "invoice_settings": {
-    "custom_fields": null,
-    "default_payment_method": null,
-    "footer": null,
-    "rendering_options": null
-  },
-  "livemode": false,
-  "metadata": {
-    "order_id": "6735"
-  },
-  "name": "Jenny Rosen",
-  "next_invoice_sequence": 1,
-  "phone": null,
-  "test_clock": null
-}`;
 
 export function ApiContentList({ articles, title }: ApiContentListProps) {
   return (
@@ -41,88 +15,14 @@ export function ApiContentList({ articles, title }: ApiContentListProps) {
       <H2>{title}</H2>
       {articles.map((article) =>
         ["Query", "Mutation"].includes(article.type) ? (
-          <ActionListItem article={article} />
+          <ApiContentListItemAction
+            key={article.id}
+            definition={article.definition as APITypeField}
+          />
         ) : (
-          <TypeListItem article={article} />
+          <ApiContentListItemType key={article.id} definition={article.definition as APIType} />
         ),
       )}
     </Grid>
   );
-}
-
-function ActionListItem({ article }: { article: APIDocsArticle }) {
-  const definition = article.definition as APITypeField;
-  const title = definition.description || "";
-  const subtitle = "Parameters";
-  const fields =
-    definition.arguments && definition.argumentNames
-      ? definition.argumentNames.map((argumentName) => definition.arguments![argumentName])
-      : [];
-  const example = buildExample(fields);
-
-  return (
-    <ApiContentListItem key={article.id} example={example} subtitle={subtitle} title={title}>
-      {fields.map((field) => (
-        <ApiContentListItemField
-          key={article.id + field.name}
-          description={field.description}
-          name={field.name}
-          type={field.type}
-        />
-      ))}
-    </ApiContentListItem>
-  );
-}
-
-function TypeListItem({ article }: { article: APIDocsArticle }) {
-  const definition = article.definition as APIType;
-  const title = definition.description || "";
-  const subtitle = "Attributes";
-  const fields =
-    definition.fields && definition.fieldNames
-      ? definition.fieldNames.map((fieldName) => definition.fields![fieldName])
-      : [];
-  const example = buildExample(fields);
-
-  return (
-    <ApiContentListItem key={article.id} example={example} subtitle={subtitle} title={title}>
-      {fields.map((field) => (
-        <ApiContentListItemField
-          key={article.id + field.name}
-          description={field.description}
-          name={field.name}
-          type={field.type}
-        />
-      ))}
-    </ApiContentListItem>
-  );
-}
-
-function buildExample(fields: APITypeFieldArgument[] | APITypeField[]) {
-  let example: Record<string, any> = {};
-
-  fields.forEach((field) => {
-    const value = (() => {
-      switch (field.type) {
-        case "ID":
-          return "id";
-        case "UUID":
-          return uuid4();
-        case "Int":
-          return 42;
-        case "String":
-          return "string";
-        case "Boolean":
-          return true;
-        case "DateTime":
-          return new Date().toISOString();
-        default:
-          return null;
-      }
-    })();
-
-    example[field.name] = value;
-  });
-
-  return JSON.stringify(example, null, 2);
 }
