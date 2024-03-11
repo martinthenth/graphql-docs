60af66147a56c4335d0b371df3b9e24ee080f850
Martin Nijboer <mrnijboer@gmail.com>
2024-03-11 02:03:29 +0100

    Replace stone color with zinc color

--- app/components/ui/code.tsx
+++ app/components/ui/code.tsx
@@ -4,7 +4,7 @@ interface CodeProps extends HTMLAttributes<HTMLHeadingElement> {}
 
 export function Code({ children, ...props }: CodeProps) {
   return (
-    <code className="p-1 rounded bg-stone-200" style={{ fontSize: "90%" }} {...props}>
+    <code className="p-1 rounded bg-zinc-200" style={{ fontSize: "90%" }} {...props}>
       {children}
     </code>
   );
